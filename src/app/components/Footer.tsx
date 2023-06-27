fff32f275c83a2d3c23b71c8ca3fe50a3b196dd6
김용민 <dydals3440@gmail.com>
2023-06-28 02:01:38 +0900

    feat: page layout

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Footer() {
+  return <div>Yongmin Blog | All Right Reserved</div>;
+}
---
+++ src/app/contact/page.tsx
@@ -0,0 +1,3 @@
+export default function ContactPage() {
+  return <p>연락하세요</p>;
+}
