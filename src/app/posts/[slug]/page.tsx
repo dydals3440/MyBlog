55dadd5f044de48800447f4fcb4f0ead455fc164
김용민 <dydals3440@gmail.com>
2023-06-28 15:21:21 +0900

    feat: react-markdown 적용 및, tailwind 추가 세팅

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,16 +1,20 @@
+import MarkdownViewer from '@/app/components/MarkdownViewer';
 import { getPostData } from '@/service/post';
 import React from 'react';
+
 type Props = {
   params: {
     slug: string;
   };
 };
+
 export default async function PantsPage({ params: { slug } }: Props) {
   const post = await getPostData(slug);
   return (
     <>
       <h1>{post.title}</h1>
-      <pre>{post.content}</pre>
+
+      <MarkdownViewer content={post.content} />
     </>
   );
 }
